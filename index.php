59c5da5016cc310babd38c0735ed5095a9bb322e
ingasafranova <sfrnv@yahoo.co.uk>
2024-07-30 08:26:44 +0300

    learn to refactor

--- index.php
+++ index.php
@@ -16,6 +16,40 @@
 </head>
 
 <body>
+  <?php
+  $MyBooks = [
+    [
+      'author' => 'J.K.Rowling',
+      'name' => 'Philosopher`s stone',
+      'character' => 'Harry Potter',
+      'releaseYear' => 1997
+    ],
+    [
+      'author' => 'J.K.Rowling',
+      'name' => 'Chamber of Secrets',
+      'character' => 'Harry Potter',
+      'releaseYear' => 1998
+    ],
+    [
+      'author' => 'J.K.Rowling',
+      'name' => 'Prisoner of Azkaban',
+      'character' => 'Harry Potter',
+      'releaseYear' => 1999
+    ],
+    [
+      'author' => 'J.Connolly',
+      'name' => 'Every dead thing',
+      'character' => 'Charlie Parker',
+      'releaseYear' => 1999
+    ],
+    [
+      'author' => 'J.Connolly',
+      'name' => 'Dark Hollow',
+      'character' => 'Charlie Parker',
+      'releaseYear' => 2000
+    ]
+  ]
+    ?>
   <h1>
     <!-- Writing string and concantenating -->
     <?php
@@ -94,69 +128,148 @@
 
   <!-- Functions and Filters -->
   <h3>My Books</h3>
-  <?php
-$MyBooks = [
-  [
-    'author'=> 'J.K.Rowling',
-    'name'=> 'Philosopher`s stone',
-    'character'=> 'Harry Potter',
-    'releaseYear'=> 1997
-  ],
-  [
-      'author' => 'J.K.Rowling',
-      'name' => 'Chamber of Secrets',
-      'character' => 'Harry Potter',
-      'releaseYear' => 1998
-  ],
-  [
-      'author' => 'J.K.Rowling',
-      'name' => 'Prisoner of Azkaban',
-      'character' => 'Harry Potter',
-      'releaseYear' => 1999
-  ],
-  [
-      'author' => 'J.Connolly',
-      'name' => 'Every dead thing',
-      'character' => 'Charlie Parker',
-      'releaseYear' => 1999
-  ],
-  [
-      'author' => 'J.Connolly',
-      'name' => 'Dark Hollow',
-      'character' => 'Charlie Parker',
-      'releaseYear' => 2000
-  ]
-]
-  ?>
+  
   <ul>
-  <?php 
-  foreach ($MyBooks as $book): ?>
-  <?php if($book['character'] === 'Charlie Parker'): ?>
-  <li>
-<?= $book['name'] ?>
-  </li>
-<?php endif ?>
-  <?php endforeach ?>
+    <?php
+    foreach ($MyBooks as $book): ?>
+      <?php if ($book['character'] === 'Charlie Parker'): ?>
+        <li>
+          <?= $book['name'] ?>
+        </li>
+      <?php endif ?>
+    <?php endforeach ?>
   </ul>
-  <?php 
-  function filterByAuthor($MyBooks,$author){
+  <?php
+  function filterByYear($MyBooks, $Year)
+  {
+    $FilteredBooks = [];
+    foreach ($MyBooks as $book) {
+      if ($book['releaseYear'] === $Year) {
+        $FilteredBooks[] = $book;
+      }
+    }
+    return $FilteredBooks;
+  }
+  ?>
+  <?php
+  function filterByAuthor($MyBooks, $author)
+  {
     $filteredBooks = [];
-    foreach($MyBooks as $book){
+    foreach ($MyBooks as $book) {
       if ($book['author'] === $author) {
         $filteredBooks[] = $book;
       }
-      }
-    return $filteredBooks;
     }
+    return $filteredBooks;
+  }
   ?>
   <ul>
-    <?php 
-    foreach(filterByAuthor($MyBooks,'J.K.Rowling' ) as $book): ?>
-    <li>
-      <?= $book['name']?>
+    <?php
+    foreach (filterByAuthor($MyBooks, 'J.K.Rowling') as $book): ?>
+      <li>
+        <?= $book['name'] ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+  <ul>
+    <?php foreach (filterByYear($MyBooks, 2000) as $book): ?>
+      <li>
+        <?= $book['name'] ?>, released <?= $book['releaseYear'] ?>
+      <?php endforeach; ?>
     </li>
-    <?php endforeach;  ?>
-    </ul>
+  </ul>
+
+  <!-- Lambda functions and refactoring -->
+  <?php
+  $filterByAuthor = function ($MyBooks, $author) {
+    $filteredBooks = [];
+    foreach ($MyBooks as $book) {
+      if ($book['author'] === $author) {
+        $filteredBooks[] = $book;
+      }
+
+    }
+    return $filteredBooks;
+  };
+
+   $filteredBooks = $filterByAuthor($MyBooks, 'J.Connolly');
+ 
+  ?>
+  <h4>Filtered Books</h4>
+  <ul>
+    <?php foreach ($filteredBooks as $book): ?>
+      <li> book
+        <?= $book['name']; ?>
+
+      </li>
+    <?php endforeach; ?>
+  </ul>
+
+  <!-- Refactoring -->
+  <?php
+ function filter ($items, $key, $value) {
+    $filteredItems = [];
+    foreach ($items as $item) {
+      if ($item[$key] === $value) {
+        $filteredItems[] = $item;
+      }
+
+    }
+    return $filteredItems;
+  };
+
+  $filteredBooks = filter($MyBooks,'author','J.K.Rowling');
+  ?>
+  <ul>
+    <?php foreach ($filteredBooks as $book): ?>
+      <li> book
+        <?= $book['name']; ?>
+  
+      </li>
+    <?php endforeach; ?>
+  </ul>
+  <?php 
+  // function filterItems ($items, $fn) {
+  //   $filteredItems = [];
+  //   foreach ($items as $item) {
+  //     if ($fn($item)) {
+  //       $filteredItems[] = $item;
+  //     }
+
+  //   }
+  //   return $filteredItems;
+  // };
+
+  // $filteredBooks = filterItems($MyBooks, function ($book) {
+  //   return $book['releaseYear'] < 2000;
+  // });
+  ?>
+  <?php 
+  // function array_filter($items, $fn)
+  // {
+  //   $filteredItems = [];
+  //   foreach ($items as $item) {
+  //     if ($fn($item)) {
+  //       $filteredItems[] = $item;
+  //     }
+
+  //   }
+  //   return $filteredItems;
+  // }
+  // ;
+
+  $filteredBooks = array_filter($MyBooks, function ($book) {
+    return $book['releaseYear'] < 2000 && $book['author'] === 'J.K.Rowling';
+  });
+  ?>
+  <h4>Refactored Books</h4>
+  <ul>
+    <?php foreach($filteredBooks as $book): ?>
+      <li>
+        <?= $book['name'] ?>
+      </li>
+      <?php endforeach; ?>
+  </ul>
 </body>
 
 </html>
